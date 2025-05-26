edd7d1b5e9bd30bd40c0241bf0f1fea675235127
hungsean <hungsean0419@gmail.com>
2025-05-26 14:05:26 +0800

    設計background 樣板

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,45 +1,41 @@
 import styled from "styled-components";
-import bgImg from "@/assets/white.png";
+import bgImg from "@/assets/forest-image.jpg";
 import avatarImg from "@/assets/avatar.png"
 
-interface BgContainerProps {
-    $bg: string;
-}
 
 // 背景層
-const BgContainer = styled.div<BgContainerProps>`
-    position: relative;
-    min-height: 100vh;
-    width: 100%;
-    background-image:
-      linear-gradient(
-        to bottom,
-        rgba(0,0,0,0)    0%,
-        rgba(0,0,0,0)   40%,
-        rgba(0,0,0,0.25)50%,
-        rgba(0,0,0,0.5) 60%,
-        rgba(0,0,0,0.5)100% 
-      ),
-      url(${props => props.$bg});
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    overflow: hidden;
+const BgWrapper = styled.div`
+  position: relative;
+  min-height: 60vh;
+  width: 100%;
+  background-color: pink;
+  overflow: hidden;
+`;
+
+const BgImage = styled.div`
+  height: 50vh;
+  width: 100%;
+  background-image: url(${bgImg});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  mask-image: linear-gradient(to bottom, black 80%, transparent 100%);
+  -webkit-mask-image: linear-gradient(to bottom, black 80%, transparent 100%);
 `;
 
 
 
-// 內容層：整個區塊置中
-const Content = styled.div`
-  position: absolute;
-  top: 0; left: 0;
-  width: 100%; height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;      /* 水平置中 */
-  justify-content: center;  /* 垂直置中 */
-  z-index: 1;
-`;
+// // 內容層：整個區塊置中
+// const Content = styled.div`
+//   position: absolute;
+//   top: 0; left: 0;
+//   width: 100%; height: 100%;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;      /* 水平置中 */
+//   justify-content: center;  /* 垂直置中 */
+//   z-index: 1;
+// `;
 
 // 頭貼樣式
 const Avatar = styled.img`
@@ -51,44 +47,50 @@ const Avatar = styled.img`
   box-shadow: 0 4px 12px rgba(0,0,0,0.3);
 `;
 
-// 社群連結容器
-const SocialLinks = styled.div`
-  display: flex;
-  gap: 16px; /* 各連結之間的間距 */
-`;
+// // 社群連結容器
+// const SocialLinks = styled.div`
+//   display: flex;
+//   gap: 16px; /* 各連結之間的間距 */
+// `;
 
-// 單一社群連結樣式
-const SocialLink = styled.a`
-  color: white;
-  font-size: 20px;
-  text-decoration: none;
-  &:hover {
-    opacity: 0.8;
-  }
-`;
+// // 單一社群連結樣式
+// const SocialLink = styled.a`
+//   color: white;
+//   font-size: 20px;
+//   text-decoration: none;
+//   &:hover {
+//     opacity: 0.8;
+//   }
+// `;
 
 
 function Home() {
     return (
-        <BgContainer $bg={bgImg}>
-            <Content>
-                {/* 正中央的頭貼 */}
-                <Avatar src={avatarImg} alt="主人頭貼" />
+        // <BgContainer $bg={bgImg}>
+        //     <Content>
+        //         {/* 正中央的頭貼 */}
+        //         <Avatar src={avatarImg} alt="主人頭貼" />
 
-                {/* 放在頭貼下方的社群連結 */}
-                <SocialLinks>
-                    <SocialLink href="https://twitter.com/你的帳號" target="_blank" rel="noreferrer">
-                        Twitter
-                    </SocialLink>
-                    <SocialLink href="https://github.com/你的帳號" target="_blank" rel="noreferrer">
-                        GitHub
-                    </SocialLink>
-                    <SocialLink href="https://www.linkedin.com/in/你的帳號" target="_blank" rel="noreferrer">
-                        LinkedIn
-                    </SocialLink>
-                </SocialLinks>
-            </Content>
-        </BgContainer>
+        //         {/* 放在頭貼下方的社群連結 */}
+        //         <SocialLinks>
+        //             <SocialLink href="https://twitter.com/你的帳號" target="_blank" rel="noreferrer">
+        //                 Twitter
+        //             </SocialLink>
+        //             <SocialLink href="https://github.com/你的帳號" target="_blank" rel="noreferrer">
+        //                 GitHub
+        //             </SocialLink>
+        //             <SocialLink href="https://www.linkedin.com/in/你的帳號" target="_blank" rel="noreferrer">
+        //                 LinkedIn
+        //             </SocialLink>
+        //         </SocialLinks>
+        //     </Content>
+      // </BgContainer>
+      <BgWrapper>
+        <BgImage>
+
+        </BgImage>
+      </BgWrapper>
+
     );
 }
 
